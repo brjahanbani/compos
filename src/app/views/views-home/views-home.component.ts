a5edabb5a92912420a6af104fbfca325c3e7650f
Babak <babak.jahanbani@yahoo.com>
2022-02-19 19:16:19 +0330

    Item List

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  styleUrls: ['./views-home.component.css'],
 })
 export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of users' },
+    { value: 48, label: '# likes' },
+    { value: 13, label: '# comments' },
+  ];
+  items = [
+    {
+      image: 'assets/images/Cadillac.jpg',
+      title: 'Cadillac',
+      description: 'Final Destination',
+      additional: 'Cadillac Escalade ESV Premium Luxury Platinum 2022',
+    },
+    {
+      image: 'assets/images/Jeep.jpg',
+      title: 'Jeep',
+      description: 'Close Enough',
+      additional: 'Jeep Wrangler Unlimited Sahara 2022',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
