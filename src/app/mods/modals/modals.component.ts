75ee10184d0cd7b4089bb891118e8e1f50680044
Babak <babak.jahanbani@yahoo.com>
2022-02-20 20:21:44 +0330

    ElementRef + ngOnInit  > Comp to app-root

--- src/app/mods/modals/modals.component.ts
+++ src/app/mods/modals/modals.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modals',
   templateUrl: './modals.component.html',
-  styleUrls: ['./modals.component.css']
+  styleUrls: ['./modals.component.css'],
 })
 export class ModalsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
-
 }
